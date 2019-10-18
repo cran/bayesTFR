10af0a1aea36f0732766eab7d1ee1c20936dbbf6
Hana Sevcikova <hanas@uw.edu>
2019-10-18 21:20:17 +0000

    version 6.4-0

--- tests/run_tests.R
+++ tests/run_tests.R
@@ -2,7 +2,7 @@ library(bayesTFR)
 source('test_functions.R')
 
 cran <- TRUE
-for(wpp in c(2010, 2012, 2015, 2017)) {
+for(wpp in rev(c(2010, 2012, 2015, 2017, 2019))) {
 	test.load.UNtfr(wpp)
 	test.load.UNlocations(wpp)
 	test.create.tfr.matrix(wpp)
@@ -29,4 +29,5 @@ if(!cran) {
 	test.run.mcmc.simulation.auto()
 	test.run.mcmc.simulation.auto.parallel()
 	test.plot.all()
+	test.reproduce.simulation()
 }
